9da3f984cb77cc9634b2ea18947275e671b4296d
SHANE <snewell@stanford.edu>
2016-05-09 11:01:33 -0700

    Giants suck

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__) 
+if __name__ == '__main__':
+    # Fire up the Flask test server
+    app.run(debug=True, use_reloader=True)
+    
+@app.route("/")
+def index():
+    template = 'index.html'
+    return render_template(template)
